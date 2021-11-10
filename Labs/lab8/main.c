705579cf328b5f13fecd31add71716736cc7c89a
student <student@PC-630.urel.feec.vutbr.cz>
2021-11-10 13:08:01 +0100

    üpdate

---
+++ Labs/lab8/main.c
@@ -0,0 +1,18 @@
+/*
+ * lab8.c
+ *
+ * Created: 10/11/2021 13:03:55
+ * Author : student
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
